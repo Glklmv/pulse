aefe4ec920aea5b0b839db87e46089629e67fbc6
Gleb <glklimov@gmail.com>
2022-10-02 21:53:30 +0300

    change - slider -tinySlider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,34 @@
-$(document).ready(function(){
-    $('.carousel-inner').slick({
-        speed: 300,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left-arr.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-arr.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                dots: false,
-                arrows: false
-                }
-            }
-        ]
-        });
+// $(document).ready(function(){
+//     $('.carousel-inner').slick({
+//         speed: 300,
+//         /* adaptiveHeight: true, */
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left-arr.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-arr.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                 dots: false,
+//                 arrows: false
+//                 }
+//             }
+//         ]
+//         });
+//     });
+
+
+
+const slider = tns({
+    container: '.carousel-inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
     });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+    });    
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+    };        
